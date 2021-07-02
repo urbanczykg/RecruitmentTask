d34bfb43f28330b6cbd68ab3a1b71e235d7b717f
Gabriela <gabriela.urbanczyk2000@gmail.com>
2021-07-02 15:27:51 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -6,26 +6,11 @@ import axios from 'axios';
 import FetchApi from './components/FetchApi'
 
 function App() {
-  const Url = 'https://vpic.nhtsa.dot.gov/api/vehicles/getmodelsformake/honda?format=json'
-  const [vehicle,setVehicle] = useState({})
-
   
-  useEffect(() => {
-    axios.get(Url)
-      .then(respose =>
-        {
-          setVehicle(respose.data)
-        })
-
-  
-  }, [Url])
-  console.log("xd");
   return (
     <div>
       <Header />
-      {console.log("xd")}
-      <h1>{vehicle.Make_Name}</h1>
-      {console.log("xd")}
+  
       <FetchApi/>
     </div>
   )
